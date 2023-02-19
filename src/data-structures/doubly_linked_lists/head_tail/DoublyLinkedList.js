2a40201868750eadc93a15e42069ed02fba6ca0b
Aumin Patel <apatel@alpha-sense.com>
2023-02-19 13:06:51 +0530

    chore: stack_queue

--- src/data-structures/doubly_linked_lists/head_tail/DoublyLinkedList.js
+++ src/data-structures/doubly_linked_lists/head_tail/DoublyLinkedList.js
@@ -23,7 +23,7 @@ export class DoublyLinkedList {
 
     node.previous = this.tail;
 
-    this.tail= node;
+    this.tail = node;
    
     this.size++;
     return this;
@@ -35,6 +35,7 @@ export class DoublyLinkedList {
     const node = new DoublyLinkedListNode(value)
     if(!this.head.next) {
       this.head = node;
+      this.tail = node;
       return this;
     }
 
@@ -48,6 +49,202 @@ export class DoublyLinkedList {
 
   }
 
+    /**
+   * @param {*} value
+   * @return {DoublyLinkedListNode}
+   */
+     delete(value) {
+      if (!this.head) {
+        return null;
+      }
   
+      let deletedNode = null;
+      let currentNode = this.head;
+  
+      while (currentNode) {
+        if (this.compare.equal(currentNode.value, value)) {
+          deletedNode = currentNode;
+  
+          if (deletedNode === this.head) {
+            // If HEAD is going to be deleted...
+  
+            // Set head to second node, which will become new head.
+            this.head = deletedNode.next;
+  
+            // Set new head's previous to null.
+            if (this.head) {
+              this.head.previous = null;
+            }
+  
+            // If all the nodes in list has same value that is passed as argument
+            // then all nodes will get deleted, therefore tail needs to be updated.
+            if (deletedNode === this.tail) {
+              this.tail = null;
+            }
+          } else if (deletedNode === this.tail) {
+            // If TAIL is going to be deleted...
+  
+            // Set tail to second last node, which will become new tail.
+            this.tail = deletedNode.previous;
+            this.tail.next = null;
+          } else {
+            // If MIDDLE node is going to be deleted...
+            const previousNode = deletedNode.previous;
+            const nextNode = deletedNode.next;
+  
+            previousNode.next = nextNode;
+            nextNode.previous = previousNode;
+          }
+        }
+  
+        currentNode = currentNode.next;
+      }
+  
+      return deletedNode;
+    }
+  
+    /**
+     * @param {Object} findParams
+     * @param {*} findParams.value
+     * @param {function} [findParams.callback]
+     * @return {DoublyLinkedListNode}
+     */
+    find({ value = undefined, callback = undefined }) {
+      if (!this.head) {
+        return null;
+      }
+  
+      let currentNode = this.head;
+  
+      while (currentNode) {
+        // If callback is specified then try to find node by callback.
+        if (callback && callback(currentNode.value)) {
+          return currentNode;
+        }
+  
+        // If value is specified then try to compare by value..
+        if (value !== undefined && this.compare.equal(currentNode.value, value)) {
+          return currentNode;
+        }
+  
+        currentNode = currentNode.next;
+      }
+  
+      return null;
+    }
+
+
+   /**
+   * @return {DoublyLinkedListNode}
+   */
+    deleteTail() {
+      if (!this.tail) {
+        // No tail to delete.
+        return null;
+      }
+  
+      if (this.head === this.tail) {
+        // There is only one node in linked list.
+        const deletedTail = this.tail;
+        this.head = null;
+        this.tail = null;
+  
+        return deletedTail;
+      }
+  
+      // If there are many nodes in linked list...
+      const deletedTail = this.tail;
+  
+      this.tail = this.tail.previous;
+      this.tail.next = null;
+  
+      return deletedTail;
+    }
+
+
+      /**
+   * Reverse a linked list.
+   * @returns {DoublyLinkedList}
+   */
+  reverse() {
+    let currNode = this.head;
+    let prevNode = null;
+    let nextNode = null;
+
+    while (currNode) {
+      // Store next node.
+      nextNode = currNode.next;
+      prevNode = currNode.previous;
+
+      // Change next node of the current node so it would link to previous node.
+      currNode.next = prevNode;
+      currNode.previous = nextNode;
+
+      // Move prevNode and currNode nodes one step forward.
+      prevNode = currNode;
+      currNode = nextNode;
+    }
+
+    // Reset head and tail.
+    this.tail = this.head;
+    this.head = prevNode;
+
+    return this;
+  }
+
+   /**
+   * @return {DoublyLinkedListNode}
+   */
+    deleteHead() {
+      if (!this.head) {
+        return null;
+      }
+  
+      const deletedHead = this.head;
+  
+      if (this.head.next) {
+        this.head = this.head.next;
+        this.head.previous = null;
+      } else {
+        this.head = null;
+        this.tail = null;
+      }
+  
+      return deletedHead;
+    }
+  
+    /**
+     * @return {DoublyLinkedListNode[]}
+     */
+    toArray() {
+      const nodes = [];
+  
+      let currentNode = this.head;
+      while (currentNode) {
+        nodes.push(currentNode);
+        currentNode = currentNode.next;
+      }
+  
+      return nodes;
+    }
+  
+    /**
+     * @param {*[]} values - Array of values that need to be converted to linked list.
+     * @return {DoublyLinkedList}
+     */
+    fromArray(values) {
+      values.forEach((value) => this.append(value));
+  
+      return this;
+    }
+  
+    /**
+     * @param {function} [callback]
+     * @return {string}
+     */
+    toString(callback) {
+      return this.toArray().map((node) => node.toString(callback)).toString();
+    }
+
 
 }
---
+++ src/data-structures/stacks/definition.md
@@ -0,0 +1,12 @@
+Stack
+An array-like data structure whose elements follow the LIFO rule: Last In, First Out.
+
+A stack is often compared to a stack of books on a table: the last book that's placed on the stack of books is the first one that's taken off the stack.
+
+The following are a stack's standard operations and their corresponding time complexities:
+
+Pushing an element onto the stack: O(1)
+Popping an element off the stack: O(1)
+Peeking at the element on the top of the stack: O(1)
+Searching for an element in the stack: O(n)
+A stack is typically implemented with a dynamic array or with a singly linked list.
