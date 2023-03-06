53b5432c76d90c2b848055f822c93432bc6aad6c
Aumin Patel <apatel@alpha-sense.com>
2023-03-06 12:10:33 +0530

    chore: jumpsearch

--- src/algo/search/jump/jumpSearch.js
+++ src/algo/search/jump/jumpSearch.js
@@ -1,45 +1,51 @@
-import Comparator from "../../../data-structures/comparator";
+import Comparator from '../../../utils/comparator/Comparator';
 
-export default function jumpsort(sortedArray, item, comparatorFunction) {
-
-
-  const comparator = new Comparator(comparatorFunction);
- 
-  let arraySize = sortedArray.length;
-
-  const jumpSize = Math.floor(Math.sqrt(arraySize));
-
- 
-
+/**
+ * Jump (block) search implementation.
+ *
+ * @param {*[]} sortedArray
+ * @param {*} seekElement
+ * @param {function(a, b)} [comparatorCallback]
+ * @return {number}
+ */
+export default function jumpSearch(sortedArray, seekElement, comparatorCallback) {
+  const comparator = new Comparator(comparatorCallback);
+  const arraySize = sortedArray.length;
 
   if (!arraySize) {
     // We can't find anything in empty array.
     return -1;
   }
- // Find the block where the seekElement belong to.
- let blockStart = 0;
- let blockEnd = jumpSize;
 
+  // Calculate optimal jump size.
+  // Total number of comparisons in the worst case will be ((arraySize/jumpSize) + jumpSize - 1).
+  // The value of the function ((arraySize/jumpSize) + jumpSize - 1) will be minimum
+  // when jumpSize = √array.length.
+  const jumpSize = Math.floor(Math.sqrt(arraySize));
 
+  // Find the block where the seekElement belong to.
+  let blockStart = 0;
+  let blockEnd = jumpSize;
+  while (comparator.greaterThan(seekElement, sortedArray[Math.min(blockEnd, arraySize) - 1])) {
+    // Jump to the next block.
+    blockStart = blockEnd;
+    blockEnd += jumpSize;
 
- while (comparator.greaterThan(item , sortedArray[Math.min(blockEnd, arraySize )-1])) {
-  blockStart = blockEnd;
-  blockEnd += jumpSize;
-
-
-  if (blockStart > arraySize) {
-    return -1;
+    // If our next block is out of array then we couldn't found the element.
+    if (blockStart > arraySize) {
+      return -1;
+    }
   }
 
- }
-
+  // Do linear search for seekElement in subarray starting from blockStart.
   let currentIndex = blockStart;
   while (currentIndex < Math.min(blockEnd, arraySize)) {
-    if (comparator.equal(sortedArray[currentIndex], item)) {
+    if (comparator.equal(sortedArray[currentIndex], seekElement)) {
       return currentIndex;
     }
 
     currentIndex += 1;
   }
+
   return -1;
 }
