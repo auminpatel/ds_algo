59d9182236d841ca4d94722537e1d3f2b76b1f0d
Aumin Patel <apatel@alpha-sense.com>
2023-03-26 20:23:01 +0530

    chore: isPalindrome

---
+++ src/algo/string/palindrome/isPalindrome.js
@@ -0,0 +1,15 @@
+export function isPalindrome( string) {
+
+  let left = 0;
+  let right = string.length -1 
+  
+  while(left<right) {
+    if(string[left]!==string[right]) {
+      return false;
+    }
+
+    left = left+1;
+    right = right-1
+  }
+  return true
+}
